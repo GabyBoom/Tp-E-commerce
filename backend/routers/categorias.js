29da75874660f665088f2e0181b94ff01a503e95
Gabriel <gaby_lamuerte@hotmail.com>
2023-11-01 11:10:17 -0300

    version 1.6

--- backend/routers/categorias.js
+++ backend/routers/categorias.js
@@ -5,11 +5,18 @@ const router = express.Router();
 router.get(`/`, async (req, res) => {
     const categoriaLista = await Categoria.find();
 
-    if(!categoriaLista) {
+    if(!categoriaLista)
         res.status(500).json({success: false});
-    }
-    res.send(categoriaLista);
+        res.status(200).send(categoriaLista);
 });
+
+router.get('/:id', async(req, res) => {
+    const categoria = await Categoria.findById(req.params.id);
+
+    if(!categoria) 
+        res.status(500).json({message: 'La categoria con ese ID no fue encontrada'});
+        res.status(200).send(categoria);
+})
 
 router.post('/', async (req, res) => {
     let categoria = new Categoria({
@@ -20,10 +27,25 @@ router.post('/', async (req, res) => {
     categoria = await categoria.save();
 
     if(!categoria) 
-    return res.status(404).send('La categoria no puede ser creada');
-
+    return res.status(400).send('La categoria no puede ser creada');
     res.send(categoria);
-})
+});
+
+router.put('/:id', async (req, res) => {
+    const categoria = await Categoria.findByIdAndUpdate(
+        req.params.id, 
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color,
+        },
+        {new: true}
+    )
+
+    if(!categoria) 
+        return res.status(400).send('La categoria no existe');
+        res.send(categoria);
+});
 
 router.delete('/:id', (req, res) => {
     Categoria.findByIdAndRemove(req.params.id).then(categoria => {
--- backend/routers/productos.js
+++ backend/routers/productos.js
@@ -1,3 +1,4 @@
+const { Categoria } = require('../models/categoria');
 const {Producto} = require('../models/producto');
 const express = require('express');
 const router = express.Router();
@@ -11,21 +12,30 @@ router.get(`/`, async (req, res) => {
     res.send(productoLista);
 });
 
-router.post(`/`, (req, res) => {
+router.post(`/`, async (req, res) => {
+    const categoria = await Categoria.findById(req.body.category);
+    if(!categoria) return res.status(400).send('Categoria Invalida')
+
     const producto = new Producto ({
         name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
         image: req.body.image,
-        countInStock: req.body.countInStock
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
     })
 
-    producto.save().then((crearProducto=>{
-        res.status(201).json(crearProducto)
-    })).catch((err)=>{
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
+
+    const productoGuardado = await producto.save();
+
+    if(!producto)
+    return res.status(500).send('El producto no puede ser creado');
+    res.send(producto);
 });
 
 module.exports = router;
